acdcdbd738e77d687afebc5f1398d70abc7b2d4c
Kateve52911 <kateve52911@stud.noroff.no>
2026-01-27 13:03:03 +0100

    installed a dev dependenvy for types/bootstrap, and created eventlistener for deleting post

--- src/app/components/listings/card/createDashboardListingCard.ts
+++ src/app/components/listings/card/createDashboardListingCard.ts
@@ -1,7 +1,9 @@
 import { Listing } from '../../../utils/helpers/card/type/listing.ts';
 import { createBaseListingCard } from '../../../utils/helpers/card/createBaseListingCard.ts';
+import { renderConfirmActionModal } from '../../../utils/helpers/modal/renderAlertModal.ts';
+import { deleteListing } from '../../../api/listings/delete/deleteListing.ts';
+import { appendAlertAndRedirect } from '../../errorHandling/newAlert/appendAlertAndRedirect.ts';
 import { appendAlert } from '../../errorHandling/newAlert/newAlert.ts';
-//import { deleteListing } from '../../../api/listings/delete/deleteListing.ts';
 
 export function createDashboardListingCard(data: Listing) {
   const { column, card } = createBaseListingCard(data);
@@ -23,9 +25,26 @@ export function createDashboardListingCard(data: Listing) {
   deleteButton.textContent = 'Delete';
   deleteButton.id = 'deleteButton';
 
-  deleteButton.addEventListener('click', () => {
-    appendAlert('Are you sure you want to delete this listing?', 'warning');
-    //deleteListing(data.id);
+  deleteButton.addEventListener('click', async () => {
+    try {
+      await renderConfirmActionModal(
+        'Are you sure you want to delete this listing?',
+        'Delete listing',
+        'Yep, delete!',
+      );
+      try {
+        await deleteListing(data.id);
+        await appendAlertAndRedirect(
+          'Post successfully deleted',
+          'success',
+          '/profile.html',
+        );
+      } catch {
+        appendAlert('Could not delete this listing', 'warning');
+      }
+    } catch {
+      // User cancelled - no action needed
+    }
   });
 
   buttonDiv.appendChild(alertDiv);
