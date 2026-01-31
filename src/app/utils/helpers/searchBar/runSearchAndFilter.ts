36f93258c4b90f8f6ae196e2391eb8bb0f170e55
Kateve52911 <kateve52911@stud.noroff.no>
2026-01-31 14:58:16 +0100

    deleted unused file

--- src/app/utils/helpers/searchBar/runSearchAndFilter.ts
+++
@@ -1,7 +0,0 @@
-import { fetchSearchBarInput } from './getSearchBarInput.ts';
-import { filterSearchResults } from '../../../components/serachAndFilter/filterSearchResults.ts';
-
-export async function runSearchAnfFilter() {
-  fetchSearchBarInput();
-  await filterSearchResults();
-}
