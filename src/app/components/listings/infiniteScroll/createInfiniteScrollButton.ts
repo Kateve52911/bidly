2beaa461ddf360d0d882429699895501e456eb3a
Kateve52911 <kateve52911@stud.noroff.no>
2026-02-01 10:16:14 +0100

    removed a container from createInfiniteScrollButton funcion as i realised it is unnessesary

--- src/app/components/listings/infiniteScroll/createInfiniteScrollButton.ts
+++ src/app/components/listings/infiniteScroll/createInfiniteScrollButton.ts
@@ -1,14 +1,8 @@
 export function createInfiniteScrollButton() {
-  const controlsContainer: HTMLDivElement = document.createElement('div');
-  controlsContainer.className = 'd-flex justify-content-center gap-3';
-  controlsContainer.id = 'controls-container';
-
   const loadMoreButton: HTMLButtonElement = document.createElement('button');
   loadMoreButton.id = 'load-more-button';
   loadMoreButton.className = 'btn btn-primary';
   loadMoreButton.innerHTML = 'Load more listings ...';
 
-  controlsContainer.append(loadMoreButton);
-
-  return controlsContainer;
+  return loadMoreButton;
 }
