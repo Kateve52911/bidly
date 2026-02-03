ea1d2d52f499392d88b1af3b728fb42b7850776e
Kateve52911 <kateve52911@stud.noroff.no>
2026-02-03 17:27:48 +0100

    updated initNavbar to see if user is logged in before adding profile page

--- src/app/components/navbar/initNavBar.ts
+++ src/app/components/navbar/initNavBar.ts
@@ -1,32 +1,37 @@
 import { renderNavBarButton } from './renderNavBarButton.ts';
 import { renderCurrentUserToNavBar } from './renderCurrentUserToNavBar.ts';
+import { isAuthenticated } from '../../utils/auth/auth.ts';
 
 export async function initNavbar() {
-  const navbarLinks: HTMLElement | null =
-    document.getElementById('navbar-links');
-
-  const dashboardLi = document.createElement('li');
-  dashboardLi.className = 'nav-item me-2';
-
-  const dashboardHref = document.createElement('a');
-  dashboardHref.href = 'profile.html';
-  dashboardHref.className = 'nav-link';
-  dashboardHref.textContent = 'Profile';
-
-  dashboardLi.appendChild(dashboardHref);
-
-  if (navbarLinks) {
-    navbarLinks.appendChild(dashboardLi);
-  }
-
   const listingsLink = document.querySelector('a[href="index.html"]');
+  if (isAuthenticated()) {
+    const navbarLinks: HTMLElement | null =
+      document.getElementById('navbar-links');
 
-  if (window.location.pathname.includes('profile.html')) {
-    dashboardHref.classList.add('active');
-    listingsLink?.classList.remove('active');
+    const dashboardLi = document.createElement('li');
+    dashboardLi.className = 'nav-item me-2';
+
+    const dashboardHref = document.createElement('a');
+    dashboardHref.href = 'profile.html';
+    dashboardHref.className = 'nav-link';
+    dashboardHref.textContent = 'Profile';
+
+    dashboardLi.appendChild(dashboardHref);
+
+    if (navbarLinks) {
+      navbarLinks.appendChild(dashboardLi);
+    }
+
+    if (window.location.pathname.includes('profile.html')) {
+      dashboardHref.classList.add('active');
+      listingsLink?.classList.remove('active');
+    } else {
+      listingsLink?.classList.add('active');
+    }
   } else {
     listingsLink?.classList.add('active');
   }
+
   renderNavBarButton();
   await renderCurrentUserToNavBar();
 }
